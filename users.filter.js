a3bfc3ebaa022254875535a0396486a90d1c7cd4
artoocampus <artoocampus@Airdiaroocampus.homenet.telecomitalia.it>
2017-03-14 14:38:57 +0100

    correzioni varie

--- users.filter.js
+++ users.filter.js
@@ -1,22 +1,23 @@
- angular.module('app').filter('filtroLingua', function(){
+ angular.module('app')
+ .filter('filterLingua', function(){
      return function(lista,lingua){
         if(lingua == "tutte"){
             return lista;
         }else{
             return lista.filter(function(el){
-                return el.lingua == lingua
-            })
+                return el.lingua == lingua;
+            });
         }
      }
 
-});
-angular.module('app').filter('filterSex',function(){
+})
+.filter('filterSex',function(){
     return function(lista,sesso){
         if(sesso == "Entrambi"){
             return lista;
         }else{
             return lista.filter(function(el){
-                return el.sesso == sesso
+                return el.sesso == sesso;
             })
         }
     }
--- users.services.js
+++ users.services.js
@@ -111,6 +111,7 @@ var utenti = [{
 
  return{
         getUtenti:getUtenti
+        
  }
 })
 
